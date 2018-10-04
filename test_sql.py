4702cee411e92711968694af50716bddcb5c4546
Milan Ševčík <majlen@civ.zcu.cz>
2018-10-04 10:13:12 +0200

    Create test data

--- test_sql.py
+++ test_sql.py
@@ -7,9 +7,12 @@ def setup_module():
 	global conn, cur
 	conn = psycopg2.connect(dbname="test", user="test", password="test", host="postgres")
 	cur = conn.cursor()
+	cur.execute("CREATE TABLE handle (handle VARCHAR(16), resource_id INTEGER)")
+	cur.execute("INSERT INTO handle (handle, resource_id) VALUES ('11025/13829', 25142)")
+	conn.commit()
 
 def test_sql_select():
-	cur.execute("select handle from handle where resource_id = 25142")
+	cur.execute("SELECT handle FROM handle WHERE resource_id = 25142")
 	result = cur.fetchone()
 	assert result[0] == '11025/13829'
 
